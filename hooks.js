8701cec11706794655743e3b2d5ec2c54d193f69
Mykhailo Budiakov <mishabudjakov@gmail.com>
2023-04-27 01:21:26 +0300

    add monster size to check distance

--- hooks.js
+++ hooks.js
@@ -6,6 +6,33 @@ function updateCombatantReactionState(combatant, newState) {
     });
 }
 
+function getEnemyDistance(tcenter, dcenter) {
+    var distance = canvas.grid.measureDistance(tcenter, dcenter);
+    return Math.floor(distance / 5) * 5
+}
+
+function getCenterObj(x, y) {
+    var a = canvas.grid.getCenter(x, y)
+    return {"x": a[0],"y": a[1]}
+}
+
+function getCenters(x, y, width) {
+    var startY = y;
+
+    var width = 2;
+    var arr = [];
+
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < width; j++) {
+            arr.push(getCenterObj(x,y));
+            y += 100;
+        }
+        y = startY;
+        x += 100;
+    }
+    return arr
+}
+
 async function postInChat(combatant) {
     const content = await renderTemplate("./modules/pf2e-reaction/templates/ask.hbs", { combatant: combatant });
     ChatMessage.create({
@@ -62,12 +89,15 @@ export default function reactionHooks() {
                 .filter((c=>c.actorId != _document.actorId && c.actor.type == filteredType && c.token.flags?.["reaction-check"]?.state))
                 .filter((cc=>cc.actor.itemTypes.action.find((feat => "attack-of-opportunity" === feat.slug))))
                 .forEach(cc => {
-                    var isReach = cc.token.actor.system.actions?.filter((e=>"strike"===e.type && e.ready))
-                    .filter((e=>e.weaponTraits.find(b=>b.name==="reach")));
-                    var distance = canvas.grid.measureDistance(cc.token.center, _document.center)
-                    distance = Math.floor(distance / 5) * 5
-                    if (distance <= (isReach.length>0?Settings.weaponReachRange:Settings.weaponRange)) {
-                        postInChat(cc);
+                    var hasStrike = cc.token.actor.system.actions?.filter((e=>"strike"===e.type && e.ready));
+                    if (hasStrike.length>0) {
+                        var isReach = hasStrike.filter((e=>e.weaponTraits.find(b=>b.name==="reach")));
+                        var canAttack = getCenters(_document.x,_document.y,_document.width)
+                            .map(a=>getEnemyDistance(cc.token.center, a))
+                            .filter(a=> (a <= (isReach.length>0?Settings.weaponReachRange:Settings.weaponRange)))
+                        if (canAttack.length>0) {
+                            postInChat(cc);
+                        }
                     }
                 })
         }
